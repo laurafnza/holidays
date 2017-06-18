04c8cecf226a91b7961a65db8be63b334d1ebebc
Unknown <franitzalaura@gmail.com>
2017-06-18 11:32:21 +0200

    HTTPS

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-$.getJSON("http://api.smartnoob.de/ferien/v1/ferien/?bundesland=nw", feriendaten => {
+$.getJSON("https://api.smartnoob.de/ferien/v1/ferien/?bundesland=nw", feriendaten => {
     var html;
     feriendaten.daten.forEach(ferien => {
         html += "<tr><td>" + ferien.title + "</td><td>" + moment.unix(ferien.beginn).format("DD.MM.YYYY") + "</td><td>" + moment.unix(ferien.ende).format("DD.MM.YYYY") + "</td></tr>";
